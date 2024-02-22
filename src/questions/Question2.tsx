9e46fa765d70855e4ca3b63029f6b35ffe05e4b7
Aydan Pirani <aydanpirani@gmail.com>
2024-02-22 01:53:08 -0600

    Added basic question template

---
+++ src/questions/Question2.tsx
@@ -0,0 +1,17 @@
+import { Box } from "@chakra-ui/react";
+import QuestionText from "../components/QuestionText";
+import QuestionOutput from "../components/QuestionOutput";
+
+export default function Question1() {
+    return (
+        <Box>
+            <QuestionText>
+                hi!
+            </QuestionText>
+
+            <QuestionOutput>
+                Output here!
+            </QuestionOutput>
+        </Box>
+    )
+};
---
+++ src/questions/Question9.tsx
@@ -0,0 +1,17 @@
+import { Box } from "@chakra-ui/react";
+import QuestionText from "../components/QuestionText";
+import QuestionOutput from "../components/QuestionOutput";
+
+export default function Question1() {
+    return (
+        <Box>
+            <QuestionText>
+                hi 9!
+            </QuestionText>
+
+            <QuestionOutput>
+                Output here!
+            </QuestionOutput>
+        </Box>
+    )
+};
